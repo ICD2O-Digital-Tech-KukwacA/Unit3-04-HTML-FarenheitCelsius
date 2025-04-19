98256c7894d858323a79eff38c3dd222203b9cde
Kukwac Adiebo <kukwac.adiebo@stu.ocsb.ca>
2025-04-19 23:11:23 +0000

    Fahrenheit to Celsius

--- js/script.js
+++ js/script.js
@@ -1,5 +1,19 @@
-// Copyright (c) 2020 Mr. Coxall All rights reserved
+// Copyright (c) 2022 Ms Raffin All rights reserved
 //
-// Created by: Mr. Coxall
-// Created on: Sep 2020
+// Created by: Kukwac
+// Created on: April 2025
 // This file contains the JS functions for index.html
+
+"use strict";
+/**
+ * This function will convert temperature in Fahrenheit to Celsius.
+ */
+function calculate() {
+  // input
+    let fahrenheit = parseFloat(document.getElementById('fahrenheit').value);
+  // process
+    let celsius = (fahrenheit - 32) * 5 / 9;
+
+  // output
+    document.getElementById('temperature').innerHTML = fahrenheit.toFixed(1) + '°F' + ' into Celsius is ' + celsius.toFixed(1) +'°C' + '.'
+}
